c46e896b409c22e2f2d9ca79f96db6ac74e1417c
JuanCirera <cirerarosa@hotmail.es>
2023-10-16 08:17:05 +0200

    feat: params for autoService in progress

--- autoService.sh
+++ autoService.sh
@@ -1,25 +1,84 @@
 #!/bin/bash
 
 #Param
-while getopts ":n:" opt; do
-  case $opt in
-    n) name="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
+# while getopts ":n:" opt; do
+#   case $opt in
+#     n) name="$OPTARG"
+#     ;;
+#     \?) echo "Invalid option -$OPTARG" >&2
+#     exit 1
+#     ;;
+#   esac
 
-  case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
-    exit 1
-    ;;
+#   case $OPTARG in
+#     -*) echo "Option $opt needs a valid argument"
+#     exit 1
+#     ;;
+#   esac
+# done
+
+
+#!/bin/bash
+
+help_content="
+Auto Service
+This script creates and enable a service in systemd
+Params:
+	-n --name 		 [Service name without spaces]
+	-d --description [Description for the service]
+	-p --path 		 [The path of the script to execute by the service]
+"
+
+# script_path="$HOME/wol/enable_wol.sh"
+# name="wol"
+# description="Enable Wake On Lan"
+
+name=$1
+description=$2
+script_path=$3
+path="/etc/systemd/system/$service_name".service
+
+while getopts ":n" opt; do
+  case $opt in
+    n)
+      param=$OPTARG
+      ;;
+    *)
+      echo "Unknown option: $opt"
+      exit 1
+      ;;
   esac
 done
-# Move file and start it
-cp ./$name.service /etc/systemd/system/
 
-systemctl daemon-reload
-systemctl enable $name.service
-systemctl start $name
-systemctl status $name
+content="
+[Unit]
+Description= $description
+
+[Service]
+Type=oneshot
+ExecStart = $script_path
+
+[Install]
+WantedBy=basic.target
+"
+
+# echo "$content" | tee "$path" > /dev/null
+
+# if [ -e $path ]; then
+#     echo "Servicio creado"
+
+#     # Recarga systemd para que tome en cuenta el nuevo servicio
+#     systemctl daemon-reload
+
+#     # Habilita y comienza el servicio
+#     systemctl enable "$name"
+#     systemctl start "$name"
+#         systemctl status "$name"
+# else
+#     echo "Error al crear el servicio"
+# fi
+
+help() {
+	echo "$help_content"
+}
+
--- auto_setup.sh
+++ auto_setup.sh
@@ -2,6 +2,13 @@
 
 # This script install and setup ubuntu server for production or devOps deploy
 
+# UFW Firewall
+enable ufw
+ufw allow 22/tcp
+
+# Wake On Lan
+ 
+
 # zsh
 apt install zsh 
 
@@ -26,18 +33,20 @@ curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/sudo.
 curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/.zshrc >> ~/.zshrc
 
 # docker & docker-compose
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # docker postinstall
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
+
+
