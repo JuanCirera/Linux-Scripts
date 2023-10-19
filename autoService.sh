17d10be6ab216ad28154c5f8de28c0d3a31e5a05
JuanCirera <cirerarosa@hotmail.es>
2023-10-19 12:55:57 +0200

    feat: params added to autoService.sh

--- autoService.sh
+++ autoService.sh
@@ -1,56 +1,6 @@
 #!/bin/bash
 
-#Param
-# while getopts ":n:" opt; do
-#   case $opt in
-#     n) name="$OPTARG"
-#     ;;
-#     \?) echo "Invalid option -$OPTARG" >&2
-#     exit 1
-#     ;;
-#   esac
-
-#   case $OPTARG in
-#     -*) echo "Option $opt needs a valid argument"
-#     exit 1
-#     ;;
-#   esac
-# done
-
-
-#!/bin/bash
-
-help_content="
-Auto Service
-This script creates and enable a service in systemd
-Params:
-	-n --name 		 [Service name without spaces]
-	-d --description [Description for the service]
-	-p --path 		 [The path of the script to execute by the service]
-"
-
-# script_path="$HOME/wol/enable_wol.sh"
-# name="wol"
-# description="Enable Wake On Lan"
-
-name=$1
-description=$2
-script_path=$3
-path="/etc/systemd/system/$service_name".service
-
-while getopts ":n" opt; do
-  case $opt in
-    n)
-      param=$OPTARG
-      ;;
-    *)
-      echo "Unknown option: $opt"
-      exit 1
-      ;;
-  esac
-done
-
-content="
+serviceContent="
 [Unit]
 Description= $description
 
@@ -62,23 +12,66 @@ ExecStart = $script_path
 WantedBy=basic.target
 "
 
-# echo "$content" | tee "$path" > /dev/null
+paramMissing=false
+path="/etc/systemd/system/$name".service
 
-# if [ -e $path ]; then
-#     echo "Servicio creado"
 
-#     # Recarga systemd para que tome en cuenta el nuevo servicio
-#     systemctl daemon-reload
-
-#     # Habilita y comienza el servicio
-#     systemctl enable "$name"
-#     systemctl start "$name"
-#         systemctl status "$name"
-# else
-#     echo "Error al crear el servicio"
-# fi
-
-help() {
-	echo "$help_content"
+function showHelp() {
+	echo "
+Auto Service
+This script creates and enable a service in systemd
+Params:
+    -n --name        [Service name without spaces]
+    -d --description [Description for the service]
+    -p --path        [The path of the script to execute by the service]
+"
 }
+
+#Param
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -n|--name)
+			name="$2"
+		shift
+		;;
+		-d|--description)
+			description="$3"
+        shift
+        ;;
+		-p|--path)
+			scriptPath="$4"
+        shift
+        ;;
+		-h|--help)
+			showHelp
+        shift
+        ;;
+        -*)
+			echo "Invalid option. Use --help for options list."
+        ;;
+    esac
+    shift
+
+done
+
+function createService(){
+    # Recarga systemd para que tome en cuenta el nuevo servicio
+    systemctl daemon-reload
+
+    # Habilita y comienza el servicio
+    systemctl enable "$name"
+    systemctl start "$name"
+	systemctl status "$name"
+
+	echo "Servicio creado con éxito"
+}
+
+if [[ -z "$name" ]] || [[ -z "$description" ]] || [[ -z "$scriptPath" ]]; then
+	paramMissing=true
+	echo "Params -n, -d and -p must be set"
+else
+	createService
+fi
 
--- auto_setup.sh
+++ auto_setup.sh
@@ -3,37 +3,37 @@
 # This script install and setup ubuntu server for production or devOps deploy
 
 # UFW Firewall
-enable ufw
-ufw allow 22/tcp
+sudo enable ufw
+sudo ufw allow 22/tcp
 
 # Wake On Lan
  
 
 # zsh
-apt install zsh 
+sudo apt install zsh 
 
 # p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 # lsd icons
 wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb /tmp
 dpkg -i /tmp/lsd_0.23.1_amd64.deb
 
 # make files for zsh plugins
-mkdir /usr/share/zsh-autosuggestions/
-touch /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/zsh-autosuggestions.zsh > /usr/share/zsh-sudo/zsh-autosuggestions.zsh
+sudo mkdir /usr/share/zsh-autosuggestions/
+sudo touch /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+sudo curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/zsh-autosuggestions.zsh > /usr/share/zsh-sudo/zsh-autosuggestions.zsh
 
-mkdir /usr/share/zsh-sudo/
-touch /usr/share/zsh-sudo/sudo.plugin.zsh
-curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/sudo.plugin.zsh > /usr/share/zsh-sudo/sudo.plugin.zsh
+sudo mkdir /usr/share/zsh-sudo/
+sudo touch /usr/share/zsh-sudo/sudo.plugin.zsh
+sudo curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/sudo.plugin.zsh > /usr/share/zsh-sudo/sudo.plugin.zsh
 
 # zshrc
 curl https://raw.githubusercontent.com/JuanCirera/Linux-Scripts/main/utils/.zshrc >> ~/.zshrc
 
 # docker & docker-compose
-apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -42,11 +42,14 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+sudo apt-get update
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # docker postinstall
-usermod -aG docker $USER
+sudo usermod -aG docker $USER
+
+# change user shell for zsh
+sudo usermod --shell /bin/bash $USER
 
 
